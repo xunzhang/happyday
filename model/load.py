f08613d5a1c88a5b8b09ce9249865038c48cd700
Lili Ma <ictmalili@gmail.com>
2016-05-06 15:41:44 +0800

    add load.py

--- model/load.py
+++ model/load.py
@@ -1,11 +1,23 @@
 import os
+import util
 import traceback
 from flask import Blueprint, render_template, request, g
 from operator import itemgetter
 
 def foo():
   v = request.form.get('load_path')
-  return v
+  print v
+#  cmd = 'ssh -i hawqdemo.pem ec2-user@ec2-52-39-229-191.us-west-2.compute.amazonaws.com "cd /tmpData/; wget %s -O tData"' % v
+  cmd = 'ssh -i hawqdemo.pem ec2-user@ec2-52-39-229-191.us-west-2.compute.amazonaws.com "cd /tmpData/; wget %s -O tData"' 
+  os.system(cmd % v)
+
+  db = util.db_connect()
+  util.createtable(db)
+  sql = "copy netflix_sample from '/tmpData/tData' delimiter ','"
+  util.db_query(db, sql)
+
+#  print v
+#  return v
 
 load = Blueprint('load', __name__, template_folder = 'templates')
 
@@ -16,4 +28,5 @@ def load_index():
 @load.route('/load/', methods = ['POST'])
 def load_submit():
     load_path = request.form.get('load_path', '')
+    foo()
     return load_path
