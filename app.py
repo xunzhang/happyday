ea10735fe5ebfd7ed6f570a546536cee93f58546
xunzhang <xunzhangthu@gmail.com>
2016-05-06 13:06:02 +0800

    goodlucj

--- app.py
+++ app.py
@@ -2,21 +2,36 @@
 # coding=utf-8
 
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from model import load
 
 app = Flask(__name__)
 app.config.from_object('setting')
 app.register_blueprint(load)
 
+port = int(os.getenv("PORT", 9099))
+
 @app.before_request
 def require_login():
     # TODO
     pass
 
+@app.route('/test')
+def foo():
+    v = request.form.get('load_path', '')
+    return v
+
+
 @app.route('/')
 def index():
+    #from pg import DB
+    #db = DB(dbname='postgres',
+    #        host='ec2-52-39-229-191.us-west-2.compute.amazonaws.com',
+    #        port=5432,user='gpadmin')
+    #db.get_tables()
+    #v = db.query('select * from t')
+    #result = v.getresult()
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=port)
--- model/load.py
+++ model/load.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, render_template
+import os
+import traceback
+from flask import Blueprint, render_template, request, g
+from operator import itemgetter
+
+def foo():
+  v = request.form.get('load_path')
+  return v
 
 load = Blueprint('load', __name__, template_folder = 'templates')
 
 @load.route('/load/')
 def load_index():
   return render_template('load.html')
+
+@load.route('/load/', methods = ['POST'])
+def load_submit():
+    load_path = request.form.get('load_path', '')
+    return load_path
